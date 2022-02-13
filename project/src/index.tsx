412c4dc95d6ba56e535d1069c4990816c4f6f576
dimonarhipon <dimonarhipon@mail.ru>
2022-02-13 14:45:59 +0300

    added components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+export const promoFilm = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={promoFilm.title}
+      genre={promoFilm.genre}
+      year={promoFilm.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
